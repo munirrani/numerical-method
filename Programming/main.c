cdfb21ac6720a8e03f4ce47c65f38a5141a661aa
Munir Mohamad Rani <munir@munirs-imac.local>
2018-09-16 20:41:47 +0800

    Initial Commit

---
+++ Programming/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Programming
+//
+//  Created by Munir Mohamad Rani on 16/09/2018.
+//  Copyright © 2018 Munir Rani. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
